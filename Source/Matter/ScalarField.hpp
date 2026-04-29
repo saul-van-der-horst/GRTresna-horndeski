823e91a702c3b1800304cf7b9c71073815bea310
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-29 07:17:38 +0100

    Fix include statement casing for CouplingAndPotential

--- Source/Matter/ScalarField.hpp
+++ Source/Matter/ScalarField.hpp
@@ -5,7 +5,7 @@
 
 #ifndef SCALARFIELD_HPP_
 #define SCALARFIELD_HPP_
-#include "CouplingandPotenetial.hpp"
+#include "CouplingAndPotenetial.hpp"
 #include "EMTensor.hpp"
 #include "FArrayBox.H"
 #include "GRParmParse.hpp"
