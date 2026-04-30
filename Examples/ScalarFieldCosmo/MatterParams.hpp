800eee64f22a03a004d9e56259951093d1159c83
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-30 18:43:59 +0100

    Add additional parameters to MatterParams structure
    Added new parameters to MatterParams structure for enhanced model configuration.

--- Examples/ScalarFieldCosmo/MatterParams.hpp
+++ Examples/ScalarFieldCosmo/MatterParams.hpp
@@ -19,6 +19,30 @@ struct params_t
     Real pi_0;
     Real dpi;
     Real scalar_mass;
+    std::string model_name;
+    Real g2;
+    Real g3;
+    Real rbs_g3;
+    Real rbs_Lambda;
+    Real nu;
+    Real Mpl;
+    Real usr_v0;
+    Real usr_y1;
+    Real kgb_usr_y2;
+    Real ga3_scalar_mass;
+    Real mu;
+    Real exph_lambda;
+    Real v;
+    Real y1;
+    Real eta;
+    Real dbin_lambda1;
+    Real dbin_Lambda;
+    Real f;
+    Real v0;
+    Real b;
+    Real Mpl;
+    Real gamma;
+    std::unique_ptr<ICouplingAndPotential> coupling;
 };
 
 inline void read_params(GRParmParse &pp, params_t &matter_params)
