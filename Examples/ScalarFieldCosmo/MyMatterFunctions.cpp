d36997bda750e8852e696ba9afa68ea40dfe0728
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-07-28 11:13:43 +0100

    Refactor coupling method calls in ScalarField

--- Examples/ScalarFieldCosmo/MyMatterFunctions.cpp
+++ Examples/ScalarFieldCosmo/MyMatterFunctions.cpp
@@ -7,45 +7,71 @@
 #include "CouplingAndPotential.hpp"
 
 
-
 Real ScalarField::V(const Real phi, const Real X) const
-{ return m_matter_params.coupling->V(phi, X); }
+{
+    return m_matter_params.coupling.V(phi, X);
+}
 
 Real ScalarField::dV_dphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->dV_dphi(phi, X); }
+{
+    return m_matter_params.coupling.dV_dphi(phi, X);
+}
 
 Real ScalarField::G2(const Real phi, const Real X) const
-{ return m_matter_params.coupling->G2(phi, X); }
+{
+    return m_matter_params.coupling.G2(phi, X);
+}
 
 Real ScalarField::dG2_dphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->dG2_dphi(phi, X); }
+{
+    return m_matter_params.coupling.dG2_dphi(phi, X);
+}
 
 Real ScalarField::dG2_dX(const Real phi, const Real X) const
-{ return m_matter_params.coupling->dG2_dX(phi, X); }
+{
+    return m_matter_params.coupling.dG2_dX(phi, X);
+}
 
 Real ScalarField::d2G2_dXX(const Real phi, const Real X) const
-{ return m_matter_params.coupling->d2G2_dXX(phi, X); }
+{
+    return m_matter_params.coupling.d2G2_dXX(phi, X);
+}
 
 Real ScalarField::d2G2_dXphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->d2G2_dXphi(phi, X); }
+{
+    return m_matter_params.coupling.d2G2_dXphi(phi, X);
+}
 
 Real ScalarField::G3(const Real phi, const Real X) const
-{ return m_matter_params.coupling->G3(phi, X); }
+{
+    return m_matter_params.coupling.G3(phi, X);
+}
 
 Real ScalarField::dG3_dphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->dG3_dphi(phi, X); }
+{
+    return m_matter_params.coupling.dG3_dphi(phi, X);
+}
 
 Real ScalarField::dG3_dX(const Real phi, const Real X) const
-{ return m_matter_params.coupling->dG3_dX(phi, X); }
+{
+    return m_matter_params.coupling.dG3_dX(phi, X);
+}
 
 Real ScalarField::d2G3_dXX(const Real phi, const Real X) const
-{ return m_matter_params.coupling->d2G3_dXX(phi, X); }
+{
+    return m_matter_params.coupling.d2G3_dXX(phi, X);
+}
 
 Real ScalarField::d2G3_dXphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->d2G3_dXphi(phi, X); }
+{
+    return m_matter_params.coupling.d2G3_dXphi(phi, X);
+}
 
 Real ScalarField::d2G3_dphiphi(const Real phi, const Real X) const
-{ return m_matter_params.coupling->d2G3_dphiphi(phi, X); }
+{
+    return m_matter_params.coupling.d2G3_dphiphi(phi, X);
+}
+
 
 Real ScalarField::my_phi_function(const RealVect &loc) const
 {
